0d8ddb652c0693f53954dc1e5cc088a5cd4c79e3
whgds1360 <maxprizrac@gmail.com>
2026-09-09 09:57:41 +0300

    Добавил окно сохранения

---
+++ PcConfigurator/windows/SaveConfiguration/SaveConfigurationWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PcConfigurator.windows.SaveConfiguration;
+
+public partial class SaveConfigurationWindow : Window
+{
+    public SaveConfigurationWindow()
+    {
+        InitializeComponent();
+    }
+}
