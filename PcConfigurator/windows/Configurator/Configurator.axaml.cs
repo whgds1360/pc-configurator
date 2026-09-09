1cba9e3b1d7d20682db8fdc67030cf419e6845e1
whgds1360 <maxprizrac@gmail.com>
2026-09-09 08:43:56 +0300

    Добавил структуру для окна конфигурации следующий коммит заполнение

---
+++ PcConfigurator/windows/Configurator/Configurator.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PcConfigurator.windows.Configurator;
+
+public partial class ConfiguratorWindow : Window
+{
+    public ConfiguratorWindow()
+    {
+        InitializeComponent();
+    }
+}
