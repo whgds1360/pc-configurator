ec2b22785f56b58ef1526a6b8c40681a5b4ea1c4
whgds1360 <maxprizrac@gmail.com>
2026-09-10 18:09:27 +0300

    Готово полностью создание структур таблиц

---
+++ PcConfigurator/helpers/TableStructures/TableStructuresCreator.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using PcConfigurator.shared.DataBase;
+
+namespace PcConfigurator.helpers.TableStructures;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Config> Configs { get; set; }
+    public DbSet<Cpu> Cpus { get; set; }
+    public DbSet<Gpu> Gpus { get; set; }
+    public DbSet<Ram> Rams { get; set; }
+    public DbSet<Motherboard> Motherboards { get; set; }
+    public DbSet<PowerUnit> PowerUnits { get; set; }
+
+
+
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (DataBaseManager.GetStatus() == OrderStatus.Connect)
+        {
+            var connectionString = DataBaseManager.GetConnectionUrl();
+            var serverVersion = ServerVersion.AutoDetect(connectionString);
+
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+        else
+        {
+            System.Diagnostics.Debug.WriteLine("Ошибка при создании структуры таблиц!");
+        }
+    }
+}
+ 
