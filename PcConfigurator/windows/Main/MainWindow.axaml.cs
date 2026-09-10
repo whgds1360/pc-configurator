2c7a59f73fd5055b9cf73df16fb512f20d6432d9
whgds1360 <maxprizrac@gmail.com>
2026-09-10 16:22:49 +0300

    фикс названий пространств имен

--- PcConfigurator/windows/Main/MainWindow.axaml.cs
+++ PcConfigurator/windows/Main/MainWindow.axaml.cs
@@ -1,11 +1,23 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using PcConfigurator.services.WindowsManager;
+using PcConfigurator.windows.Configurator;
 
 namespace PcConfigurator.windows.Main;
 
 public partial class MainWindow : Window
 {
+    Button? currentConfiguration = null;
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void CreateHandler(object? sender, RoutedEventArgs e)
+    {
+        var nextWindow = new ConfiguratorWindow();
+
+        MainWindowsManager.changeWindow(closingWindow: this, newWindow: nextWindow);
     }
 }
