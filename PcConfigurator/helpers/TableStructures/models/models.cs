1c149adb8ad99f3d9ab306a4a9807c75d93cc3cf
whgds1360 <maxprizrac@gmail.com>
2026-09-19 10:45:03 +0300

    Обновление структур таблиц БД

--- PcConfigurator/helpers/TableStructures/models/models.cs
+++ PcConfigurator/helpers/TableStructures/models/models.cs
@@ -1,5 +1,7 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace PcConfigurator.helpers.TableStructures;
 
@@ -30,7 +32,11 @@ public class Config
     public int RamId { get; set; }
     [ForeignKey(nameof(RamId))]
     public Ram Ram { get; set; } = null!;
-    
+
+    public int PcCaseId { get; set; }
+    [ForeignKey(nameof(PcCaseId))]
+    public PcCase pcCase { get; set; } = null!;    
+
     public int CoolerId { get; set; }
     [ForeignKey(nameof(CoolerId))]
     public Cooler Cooler { get; set; } = null!;
@@ -147,49 +153,6 @@ public class PowerUnit
     public string Certificate { get; set; } = string.Empty;
 }
 
-public class CpuCompatibility
-{
-    [Key]
-    public int Id { get; set; }
-
-    public int CpuId { get; set; }
-    [ForeignKey(nameof(CpuId))]
-    public Cpu Cpu { get; set; } = null!;
-
-    [MaxLength(50)]
-    public string Chipset { get; set; } = string.Empty;
-}
-
-public class PsuConnector
-{
-    [Key]
-    public int Id { get; set; }
-
-    public int PowerUnitId { get; set; }
-    [ForeignKey(nameof(PowerUnitId))]
-    public PowerUnit PowerUnit { get; set; } = null!;
-
-    [MaxLength(50)]
-    public string ConnectorType { get; set; } = string.Empty;
-
-    public int ConnectorCount { get; set; }
-}
-
-public class GpuPowerRequirement
-{
-    [Key]
-    public int Id { get; set; }
-
-    public int GpuId { get; set; }
-    [ForeignKey(nameof(GpuId))]
-    public Gpu Gpu { get; set; } = null!;
-
-    [MaxLength(50)]
-    public string ConnectorType { get; set; } = string.Empty;
-
-    public int ConnectorCount { get; set; }
-}
-
 public class Cooler
 {
     [Key]
@@ -204,4 +167,61 @@ public class Cooler
     public int TdpCooling { get; set; }
 
     public int Height { get; set; }
+}
+
+public class PcCase
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string SizeType { get; set; } = string.Empty;
+}
+
+[PrimaryKey(nameof(CaseId), nameof(MotherboardFormFactor))]
+public class PcCaseCompatibility
+{
+    public int CaseId { get; set; }
+
+    [ForeignKey(nameof(CaseId))]
+    public PcCase PcCase { get; set; } = null!;
+
+    [MaxLength(50)]
+    public string MotherboardFormFactor { get; set; } = string.Empty;
+}
+
+[PrimaryKey(nameof(CpuId), nameof(Chipset))]
+public class CpuCompatibility
+{
+    public int CpuId { get; set; }
+    [ForeignKey(nameof(CpuId))]
+    public Cpu Cpu { get; set; } = null!;
+
+    [MaxLength(50)]
+    public string Chipset { get; set; } = string.Empty;
+}
+
+[PrimaryKey(nameof(PowerUnitId), nameof(ConnectorType))]
+public class PsuConnector
+{
+    public int PowerUnitId { get; set; }
+    [ForeignKey(nameof(PowerUnitId))]
+    public PowerUnit PowerUnit { get; set; } = null!;
+
+    [MaxLength(50)]
+    public string ConnectorType { get; set; } = string.Empty;
+
+    public int ConnectorCount { get; set; }
+}
+
+[PrimaryKey(nameof(GpuId), nameof(ConnectorType))]
+public class GpuPowerRequirement
+{
+    public int GpuId { get; set; }
+    [ForeignKey(nameof(GpuId))]
+    public Gpu Gpu { get; set; } = null!;
+
+    [MaxLength(50)]
+    public string ConnectorType { get; set; } = string.Empty;
+
+    public int ConnectorCount { get; set; }
 }
